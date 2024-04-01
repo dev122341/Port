2251d73570c3a9ae7637b073399be84e913503ad
dev122341 <161851152+dev122341@users.noreply.github.com>
2024-04-01 14:12:55 -0400

    Update about-me.vue

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -162,8 +162,8 @@
             <h3 class="text-white lg:text-menu-text mb-4 text-sm">// Code snippet showcase:</h3>
 
             <div class="flex flex-col overflow-scroll">
-              <!-- snippets -->
-              <GistSnippet data-aos="fade-down" v-for="(gist, key) in config.public.dev.gists" :key="key" :id="gist" />
+              <!-- snippets 
+              <GistSnippet data-aos="fade-down" v-for="(gist, key) in config.public.dev.gists" :key="key" :id="gist" /> -->
             </div>
           </div>
 
